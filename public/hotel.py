1b2203eb9d6466f3b3c2af05812d9335d6c27a97
maksym <maxim.shaforostov@gmail.com>
2024-06-18 19:24:42 +0200

    GUI class first interpretation

--- public/hotel.py
+++ public/hotel.py
@@ -1,13 +1,18 @@
 import sqlite3
+import os
+import tkinter as tk
+from tkinter import ttk
 
 
 class Hotel:
     def __init__(self):
-        self.connect = sqlite3.connect('hotel.db')  # Connect to/create
         try:
+            self.connect = sqlite3.connect('hotel.db')  # Connect to/create
             self.cursor = self.connect.cursor()
 
             self.create_tables()
+            g = self.GUI(self)
+            # self.remove_row("Guests", [["20"], ["19"]])
             # self.update_table_row("Guests", 20, ["Liamm", "Jackson", "liam.jackson@example.com", "+421-95-162-0414"])
             # self.update_table_row("Guests", 19, ["Evelyn", "Martinez", "evelyn.martinez@example.com", None])
             # self.update_table_row("Guests", 18, [None,"Hernandez","abigail.hernandez@example.com","+421-95-867-7175"])
@@ -151,6 +156,11 @@ class Hotel:
     def delete_table(self, table):
         self.cursor.execute(f'DROP TABLE IF EXISTS {table};')
 
+    @staticmethod
+    def delete_database(db):
+        if os.path.exists(db):
+            os.remove(db)
+
     def update_table_row(self, table, row_id, new_data_array):
         table_columns = self.get_columns_names(table)
         id_name = table_columns[0]  # name of the row_id column
@@ -171,6 +181,12 @@ class Hotel:
         self.connect.execute(
             f'INSERT INTO {table} {table_columns} VALUES ({(len(table_columns) * "?, ")[:-2]})', data_array)
 
+    def remove_row(self, table, ids):  # [["20"], ["19"]]
+        table_columns = self.get_columns_names(table)
+        id_name = table_columns[0]  # name of the row_id column
+        self.connect.executemany(
+            f'DELETE FROM {table} WHERE {id_name} = ?', ids)
+
     # ------------------------------------------------------------------------------------------------------------------
     # Saving to files --------------------------------------------------------------------------------------------------
     def write_table_into_file(self, table, file_name):
@@ -180,10 +196,87 @@ class Hotel:
         for row in rows:
             b_row = b'\x00'.join([str(element).encode('utf-8') for element in row][1:]) + b'\x00\x00'
             b_data += b_row
-
+    
         with open(file_name, 'wb') as file:
             file.write(b_data)
+
     # ------------------------------------------------------------------------------------------------------------------
+    # Draw GUI ---------------------------------------------------------------------------------------------------------
+    class GUI:
+        def __init__(self, hotel_self):
+            self.hotel_self = hotel_self
+            self.root = tk.Tk()
+            self.root.title("Hotel Database")
+            self.table = None
+
+            self.create_buttons()
+            self.create_table()
+            self.fill_the_table("Guests")
+
+            self.root.mainloop()
+
+        def create_buttons(self):
+            header_frame = tk.Frame(self.root)
+            header_frame.pack(side=tk.TOP)
+
+            tk.Button(header_frame, text="Load Guests", command=self.load_guests).pack(side=tk.LEFT)
+            tk.Button(header_frame, text="Load Rooms", command=self.load_rooms).pack(side=tk.LEFT)
+            tk.Button(header_frame, text="Load Bookings", command=self.load_bookings).pack(side=tk.LEFT)
+
+        def create_table(self):
+            table_frame = tk.Frame(self.root)
+            table_frame.pack(fill=tk.BOTH, expand=True)
+
+            self.table = ttk.Treeview(table_frame)
+            self.table.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+            scrollbar = ttk.Scrollbar(table_frame, orient=tk.VERTICAL, command=self.table.yview)
+            self.table.configure()  # yscroll=scrollbar.set
+            scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        def fill_the_table(self, table):
+            self.table["columns"] = self.hotel_self.get_columns_names(table)
+            self.table.column("#0", width=0, stretch=tk.NO)  # Hide the default first column
+            self.table.heading("#0", text="", anchor=tk.W)
+
+            for col in self.table["columns"]:
+                w = 200
+                if col[-2:] == "id":
+                    w = 100
+                self.table.column(col, anchor=tk.W, width=w)
+                self.table.heading(col, text=col, anchor=tk.W)
+
+        def load_data(self, table_name, columns):
+            # Clear the current table
+            # for item in self.table.get_children():
+            #     self.table.delete(item)
+
+            # Connect to the database and fetch data
+            conn = sqlite3.connect("hotel.db")
+            cursor = conn.cursor()
+            cursor.execute(f"SELECT * FROM {table_name}")
+            rows = cursor.fetchall()
+            conn.close()
+
+            # Update the Treeview widget with new columns and data
+            self.table["columns"] = columns
+            for col in self.table["columns"]:
+                self.table.heading(col, text=col)
+
+            for row in rows:
+                self.table.insert("", tk.END, values=row)
+
+        def load_guests(self):
+            self.load_data("Guests", ("ID", "Name", "Gender", "Age"))
+
+        def load_rooms(self):
+            self.load_data("Rooms", (
+                "RoomNumber", "BedsType", "PricePerNight", "AvailabilityStatus", "BathroomType", "Balcony",
+                "SideOfBuilding"))
+
+        def load_bookings(self):
+            self.load_data("Bookings",
+                           ("BookingID", "GuestID", "RoomNumber", "CheckInDate", "NightsStayed", "CheckOutDate"))
 
 
 h = Hotel()
